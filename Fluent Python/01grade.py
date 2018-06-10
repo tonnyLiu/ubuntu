bee27b078a31d67afbc7c30de7ffba1598c8f7bb
ls <1425050808@qq.com>
2018-06-10 20:41:37 +0800

    train

--- Fluent Python/01grade.py
+++ Fluent Python/01grade.py
@@ -1,6 +1,12 @@
+#此程序是对bisect模块的应用，这是一个排序插入位置的模块
 import bisect
 
 def grade(score, grades='FDCBA'):
+    '''
+    :param score: 输入的分数
+    :param grades: 划定的等级
+    :return: 分数的等级
+    '''
     breakpoints = [60, 70, 80 ,90]
     i = bisect.bisect(breakpoints, score)
     return grades[i]
